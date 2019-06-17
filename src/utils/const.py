19cd5677cb914922ad093669caac5781a9843ee9
Ngoc-Hoang Nguyen <hoangnn90@gmail.com>
2019-06-17 15:11:23 +0700

    Update changelist description

--- src/utils/const.py
+++ src/utils/const.py
@@ -20,5 +20,22 @@ others = "others.xml"
 tag_conditions = ['DeviceType', 'SimType', 'ImsSupported', 'ProjectType', 'IMSSetting']
 
 DEFAULT_CHANGELIST_DESCRIPTION = "\
-[Description] Changelist created by CSCChangeListCreator tool \
+[Project] CSCChangeListCreator \n\
+[Title] Changelist created by CSCChangeListCreator tool \n\
+[BinType] CSC \n\
+[Customer] \n\
+            \n\
+[Issue#] \n\
+[Request] SWPL \n\
+[Occurence Version] \n\
+    \n\
+[Problem]  \n\
+[Cause & Measure]  \n\
+[Checking Method]  Build then check \n\
+    \n\
+[Team]  SVMC-CSC \n\
+[Developer]   \n\
+[Solution company] Samsung  \n\
+[Change Type] Update  \n\
+    \n\
 "
